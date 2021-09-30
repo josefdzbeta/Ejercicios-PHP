2db34b840f46cba1bd145ccdc7905435693b4e05
Jose Angel <jafb19@outlook.com>
2021-10-01 00:18:01 +0200

    ejercicios arreglados

---
+++ bdprocedimiento3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            $servidorbd='127.0.0.1';
+            $usuario='root';
+            $contraseña='';
+            $basedatos='bdprocedimiento';
+
+
+            $conexion = mysqli_connect($servidorbd, $usuario, $contraseña, $basedatos);
+
+            //$consulta='SELECT * FROM Alumnos';
+            $consulta='SELECT Id, Nombre, Repite FROM Alumnos WHERE Id=1';
+
+
+            $resultado = mysqli_query($conexion, $consulta);
+            
+            while($fila = mysqli_fetch_array($resultado)){
+                for($i=0; $i<count($fila); $i++){
+                    echo $i.'=>'.$fila[$i].'<br />';
+                }
+               echo  '<br /><br />';
+            }
+
+        ?>
+    </body>
+</html>
